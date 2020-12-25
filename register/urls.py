5a71ac794a18ba64e163b2ead29356b21d401102
Gabriele Mongiano <hom3rster@gmail.com>
2020-12-25 17:00:14 +0100

    Add variety within species, with form validations and unique_together (name and species) constraint

--- register/urls.py
+++ register/urls.py
@@ -7,6 +7,6 @@ app_name = 'register'
 urlpatterns = [
         path('species/', views.PlantSpeciesList.as_view(), name='species_list'),
         path('species/create', views.PlantSpeciesCreate.as_view(), name='species_create'),
-        path('species/<int:pk>', views.PlantSpeciesDetail.as_view(), name='plantspecie_detail'),
+        path('species/<int:pk>', views.PSDetail.as_view(), name='plantspecie_detail'),
         path('variety/<int:pk>', views.PlantVarietyDetail.as_view(), name='variety_detail'),
 ]
--- register/views.py
+++ register/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, reverse
+from django.shortcuts import get_object_or_404, reverse
 from django.http import HttpResponse
 from django.views.generic import ListView,DetailView,CreateView,View,FormView
+from django.views.generic.edit import FormMixin
 from django.views.generic.detail import SingleObjectMixin
 from .models import PlantSpecies,PlantVariety
 from .forms import PlantSpeciesForm, PlantVarietyForm
@@ -33,6 +34,36 @@ class PlantSpeciesDetail(DetailView):
                 species=self.get_object())
         new_variety.save()
         return self.get(self, request, *args, **kwargs)
+
+class PSDetail(FormMixin, DetailView):
+    model = PlantSpecies
+    form_class = PlantVarietyForm
+    template_name = "register/plantspecies_detail.html"
+    context_object_name = 'species'
+
+    def get_initial(self):
+        return {"species": self.get_object()}
+
+    def get_success_url(self):
+        return reverse("register:plantspecie_detail", kwargs={"pk": self.object.pk})
+
+    def get_context_data(self, **kwargs):
+        context = super(PSDetail, self).get_context_data(**kwargs)
+        context["form"] = self.get_form()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        plant_variety = PlantVariety(**form.cleaned_data)
+        plant_variety.save()
+        return super().form_valid(form)
 
 class PlantVarietyDetail(DetailView):
     model = PlantVariety
