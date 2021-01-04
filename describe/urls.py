513ea974ef53c279f0eb3cc91c3276225767fb71
Gabriele Mongiano <hom3rster@gmail.com>
2021-01-04 14:08:02 +0100

    Removed all the previous formsets.

--- describe/urls.py
+++ describe/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
         path('<int:pk>/', views.DescriptionDetail.as_view(), name='description_detail'),
         path('protocols/', views.ProtocolsList.as_view(), name='protocols_list'),
         path('protocols/<int:pk>/', views.ProtocolDetail.as_view(), name = 'protocol_detail'),
-        path('protocols/<int:pk>/manage', views.manage_trait, name = 'protocol-manage'),
 ]
--- describe/views.py
+++ describe/views.py
@@ -1,5 +1,3 @@
-from describe.forms import TraitFormset
-from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import ListView, DetailView
 from .models import Description, Protocol
 from register.models import PlantSpecies
@@ -29,27 +27,3 @@ class ProtocolsList(ListView):
 class ProtocolDetail(DetailView):
     model = Protocol
     context_object_name = 'protocol'
-
-
-
-def manage_trait(request, pk):
-    """
-    Edit trait and their states for a protocol.
-    https://github.com/philgyford/django-nested-inline-formsets-example/
-    """
-
-    protocol = get_object_or_404(Protocol, id=pk)
-
-    if request.method == 'POST':
-        import pdb; pdb.set_trace()
-        formset = TraitFormset(request.POST, instance=protocol)
-        if formset.is_valid():
-            formset.save()
-            return redirect('describe:protocol_detail', pk=protocol.pk)
-    else:
-        formset = TraitFormset(instance=protocol)
-
-    return render(request, 'describe/manage_trait.html', {
-                  'protocol': protocol,
-                  'trait_formset':formset}
-                  )
