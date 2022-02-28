c0d70a7a6ee3dac2eb46e06df26caf7d01559edf
Gabriele Mongiano <gabriele.mongiano@crea.gov.it>
2022-02-28 15:23:19 +0100

    crop creation API for varieties

--- calculator/views.py
+++ calculator/views.py
@@ -1,30 +1,39 @@
 from django.db.models.query_utils import Q
 from django.http.response import Http404, HttpResponseBadRequest, JsonResponse
 from django.shortcuts import render
-from register.models import PlantSpecies
+from register.models import PlantSpecies, PlantVariety
 import math
+from django.core import serializers
 
 from spaces.models import Area
 
 def get_plants_number(distb, distw, width=1):
+    if distb==0 or distw==0:
+        return (0, 0, 0)
     rows_per_ridge=math.floor(width/distb)
     rows_per_ridge=1 if rows_per_ridge < 2 else rows_per_ridge
     plants_per_row=math.floor(1/distw)
     plants_per_row=1 if plants_per_row < 2 else plants_per_row
-    return(rows_per_ridge*plants_per_row)
+    return(rows_per_ridge, plants_per_row, rows_per_ridge*plants_per_row)
 
 def calculator(request):
-    areas=Area.objects.all()
-    plantspecies=PlantSpecies.objects.all()
-    return render(request, 'calculator/index.html', { 'areas': areas, 'plantspecies' : plantspecies})
+    areas=Area.objects.values('pk', 'name')
+    plantspecies=PlantSpecies.objects.values('pk', 'common_name')
+    return render(
+        request,
+        'calculator/index.html',
+        {
+            'areas': areas,
+            'plantspecies' : plantspecies,
+        }
+    )
 
 def get_area_ajax(request):
     if request.method == 'POST':
         # Get IDs from POST request
         area_id=request.POST['area_id']
         plantspecies_id=request.POST['plantspecies_id']
-
-        # Try to fetch Area and PlantSpecies objects
+        # import pdb; pdb.set_trace()
         try:
             area=Area.objects.values('length', 'width', 'name').get(pk=area_id)
         except Area.DoesNotExist:
@@ -39,9 +48,11 @@ def get_area_ajax(request):
 
         # Area is returned as a dictionary
         json_resp=area
+        json_resp["total_area"]=round(json_resp["length"]*json_resp["width"],0)
 
         # These are the parameters being fetched from the species
         fetched_parameters = ["yield", "distw", "distb"]
+
         # Get the specified parameters for the selected plant species
         params=plantspecies.cropparameter_set.filter(parameter__code__in=fetched_parameters)
         # Now filter out the most recent parameters, at first ordering by update date
@@ -50,20 +61,35 @@ def get_area_ajax(request):
         # json response dictionary
         for prm in params.order_by('parameter__code', 'updated_at').distinct('parameter__code'):
             json_resp[prm.parameter.code]=prm.value
+
+        for prm in fetched_parameters:
+            if not prm in json_resp:
+                return JsonResponse(json_resp,safe=False)
         # Calculated values from the fetched data
-        json_resp["total_area"]=round(json_resp["length"]*json_resp["width"],0)
-        json_resp["plant_number_1m"]=get_plants_number(
+        _, _, json_resp["plant_number_1m"]=get_plants_number(
             json_resp['distb'],
             json_resp['distw']
         )
         json_resp["unit_yield"]=json_resp['yield']/json_resp['plant_number_1m']
-        json_resp["plant_number"]=get_plants_number(
+        json_resp["nrow"], json_resp["nplants"], json_resp["plant_number"]=get_plants_number(
             json_resp['distb'],
             json_resp['distw'],
             json_resp['width']
         )
         json_resp["expected_yield"]=round(json_resp["unit_yield"]*json_resp["plant_number"]*json_resp["length"],2)
         # Return data in json format
-        return JsonResponse(json_resp)
+        return JsonResponse(json_resp,safe=False)
+    else:
+        return HttpResponseBadRequest('<h1>Page not found</h1>')
+
+def get_varieties(request):
+    if request.method == 'POST':
+        if 'plantspecies_id' in request.POST:
+            try:
+                varieties_list=PlantVariety.objects.filter(species_id=request.POST['plantspecies_id']).order_by('name')
+            except:
+                raise Http404("Plant Species does not exists")
+            data = serializers.serialize('json', varieties_list)
+            return JsonResponse(data, safe=False)
     else:
         return HttpResponseBadRequest('<h1>Page not found</h1>')
