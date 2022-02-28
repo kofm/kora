835001c4cacf421c6ca2dd57f9a998b298bc5276
Gabriele Mongiano <gabriele.mongiano@crea.gov.it>
2022-02-28 15:28:00 +0100

    incomplete draft of crop models

--- calculator/models.py
+++ calculator/models.py
@@ -1,3 +1,14 @@
+"""
+Models to store informations relative to a user crop.
+
+They consist of a reference Area, a Specie/Variety combination
+"""
 from django.db import models
 
-# Create your models here.
+class Crop(models.Model):
+    """
+    Stores a specific crop in relation to a specific Area and consisting of a
+    PlantSpecies or Varieties. It can store temporary ovverides e.g. for the
+    temperature offset of the Area, or planting scheme (distw, distb)
+    """
+    notes=models.CharField(help_text="Notes relative to the Crop")
