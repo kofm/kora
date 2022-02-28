6e84fa3e2640ace66ffe22755594c58d7a6f1649
Gabriele Mongiano <gabriele.mongiano@crea.gov.it>
2022-02-28 15:23:33 +0100

    tmux script

---
+++ tmux.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+DEVENV="~/.venvs/django/bin/activate"
+
+if test -n $TMUX; then
+  SESSION=$(tmux display-message -p '#S')
+  # Name first Pane and start zsh
+  tmux rename-window -t 1 'Main'
+
+  # Check if docker is running
+  docker info || \
+    # If not, start docker daemon
+    tmux send-keys -t 'Main' 'sudo systemctl start docker' C-m
+  # Check if psql container is running
+  docker-compose ps --status running | grep persefone-db || \
+    # If not, start the container
+    tmux send-keys -t 'Main' 'docker-compose up db' C-m
+
+  tmux split-window -t 'Main'
+  tmux send-keys -t$SESSION:1.2 ". $DEVENV" C-m "python manage.py runserver" C-m
+
+  tmux new-window -t$SESSION:2 -n 'neovim'
+  tmux send-keys -t 'neovim' ". $DEVENV" C-m "nvim" C-m
+fi
+    # tmux send-keys -t 'Hugo Server' 'hugo serve -D -F' C-m # Switch to bind script?
+
+# setup Writing window
+
+# Setup an additional shell
+# tmux new-window -t $SESSION:3 -n 'Shell'
+# tmux send-keys -t 'Shell' "zsh" C-m 'clear' C-m
+
+# Attach Session, on the Main window
+# tmux attach-session -t $SESSION:0
