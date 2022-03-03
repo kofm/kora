5f26099a632d6f3fa13931d2f0efd7bddd83c3b4
Gabriele Mongiano <gabriele.mongiano@crea.gov.it>
2022-03-03 22:05:20 +0100

    cropparameter model

--- calculator/models.py
+++ calculator/models.py
@@ -6,6 +6,7 @@ They consist of a reference Area, a Specie/Variety combination
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from parameters.models import ParameterValue
 
 from spaces.models import Area
 
@@ -17,7 +18,19 @@ class Crop(models.Model):
     """
     notes=models.CharField(max_length=500, help_text="Notes relative to the Crop")
     # Relation to PlantSpecies or PlantVariety
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey('content_type', 'object_id')
+    content_type=models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id=models.PositiveIntegerField()
+    content_object=GenericForeignKey('content_type', 'object_id')
     area=models.ForeignKey(Area, on_delete=models.PROTECT,)
+
+    def __str__(self) -> str:
+        if self.content_type.model=='plantspecies':
+            return self.content_object.common_name
+        else:
+            return self.content_object.name
+
+class CropParameter(ParameterValue):
+    """
+    Stores a Species/VarietalParameter ovveride for specific Crop
+    """
+    crop=models.ForeignKey(Crop, on_delete=models.CASCADE)
