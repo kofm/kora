27dcf00daf3eb39bfbf050a0175956fd2b3cbde9
Gabriele Mongiano <gabriele.mongiano@crea.gov.it>
2022-04-24 14:45:47 +0200

    fix2 prev commit

--- calculator/urls.py
+++ calculator/urls.py
@@ -6,7 +6,7 @@ app_name = "calculator"
 
 urlpatterns = [
     path("crop/<int:pk>", views.CropDetailView.as_view(), name="crop-detail"),
-    path("area/<int:area_id>/crop/create", views.CropCreateView.as_view(), name="crop-create"),
+    path("area/<int:area_id>/crop/create", views.CropCreateView.as_view(), name="crop-create-area"),
     path("crop/create", views.CropCreateView.as_view(), name="crop-create"),
     path("crop/<int:pk>/update", views.crop_update_view, name="crop-update"),
     path("crop/<int:pk>/management/create", views.ManagementCreateView.as_view(), name="management-create"),
