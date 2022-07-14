ab53e43febf0e35b0c19d3e95a031e83b7695e24
Gabriele Mongiano <gabriele.mongiano@crea.gov.it>
2022-07-14 11:37:47 +0200

    settings more

--- persefone/settings/base.py
+++ persefone/settings/base.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
@@ -64,8 +63,6 @@ WSGI_APPLICATION = "persefone.wsgi.application"
 
 
 # Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -84,8 +81,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 # Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -97,8 +92,6 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = "static/"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
