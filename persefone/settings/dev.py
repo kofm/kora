585bff41454227d578c56088cab4e25ba39165c5
Gabriele Mongiano <gabriele.mongiano@crea.gov.it>
2023-02-22 15:24:10 +0100

    settings/dev.py to use local db

--- persefone/settings/dev.py
+++ persefone/settings/dev.py
@@ -12,7 +12,7 @@ DATABASES = {
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "db",
+        "HOST": "localhost",
         "PORT": 5432,
     },
 }
