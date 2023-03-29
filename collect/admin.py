983a4127c864088cd43bfa6c638a9c0458ec74f8
Gabriele Mongiano <gabriele.mongiano@crea.gov.it>
2023-03-29 15:39:45 +0200

    added storage and storageposition to admin

--- collect/admin.py
+++ collect/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from collect.models import Cart, CartItem
+from collect.models import Cart, CartItem, Storage, StoragePosition
 
 admin.site.register(Cart)
 admin.site.register(CartItem)
+admin.site.register(Storage)
+admin.site.register(StoragePosition)
