41290b276ecfdea5040323daf240334626d8ef61
Gabriele Mongiano <hom3rster@gmail.com>
2023-11-02 11:17:12 +0100

    Feat: django-tables2 for parameters tables

---
+++ parameters/tables.py
@@ -0,0 +1,29 @@
+import django_tables2 as tables
+
+from parameters.models import Parameter, SpeciesParameter, VarietalParameter
+
+
+class ParameterTable(tables.Table):
+    code = tables.Column(linkify=True)
+    actions = tables.TemplateColumn(
+        template_name="parameters/partials/parameter_table_actions.html",
+        verbose_name="",
+        orderable=False,
+        attrs={"td": {"class": "text-end"}},
+    )
+
+    class Meta:
+        model = Parameter
+        exclude = ("id",)
+
+class VarietalParameterTable(tables.Table):
+    class Meta:
+        model = VarietalParameter
+        fields = ("variety", "value", "note", "created_at", "updated_at")
+        empty_text = "There are no associated parameters."
+
+class SpeciesParameterTable(tables.Table):
+    class Meta:
+        model = SpeciesParameter
+        fields = ("specie", "value", "note", "created_at", "updated_at")
+        empty_text = "There are no associated parameters."
