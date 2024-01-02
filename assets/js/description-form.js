ef53fd3e03a89efcd2350b199d3f5eaf2c34afe4
Gabriele Mongiano <hom3rster@gmail.com>
2024-01-02 11:05:23 +0100

    Fix bug in create description js

--- assets/js/description-form.js
+++ assets/js/description-form.js
@@ -1,16 +1,25 @@
 // JS logic to populate the tom-select inputs in Description forms
 // Mind that the protocol restapi endpoint is stored in a `url`
 // variable assigned in the description_form.html template
+
 import "../scss/tom-select.scss";
 import TomSelect from "tom-select";
-const sources = JSON.parse(document.getElementById("sources").textContent);
 
-var getProtocols = function (id) {
+const sources = JSON.parse(document.getElementById("sources").textContent);
+let protocolTomSelect;
+
+const getProtocols = (id) => {
+
+    if (!protocolTomSelect) {
+	return;
+    }
+
     if (!id) {
 	protocolTomSelect.disable();
 	return;
     }
-    fetch(url + "&variety=" + id)
+
+    fetch(`${url}&variety=${id}`)
     .then((response) => response.json())
     .then((data) => {
         protocolTomSelect.clear();
@@ -23,20 +32,16 @@ var getProtocols = function (id) {
 const varietyTomSelect = new TomSelect("#id_variety", {
     maxItems: 1,
     create: false,
-    onChange: function (value) {
-	getProtocols(value);
-    },
+    onChange: getProtocols,
 });
 
-const protocolTomSelect = new TomSelect("#id_protocol", {
+protocolTomSelect = new TomSelect("#id_protocol", {
     maxItems: 1,
     create: false,
     valueField: "pk",
     labelField: "name",
     searchField: ["name"],
-    onInitialize: function() {
-	getProtocols(varietyTomSelect.getValue());
-    }
+    onInitialize: () => getProtocols(varietyTomSelect.getValue()),
 });
 
 new TomSelect("#id_name", {
