3bcc62c62b50480bc86d1163db16078f2fa79a98
Gabriele Mongiano <hom3rster@gmail.com>
2024-12-01 20:23:49 +0100

    fix: update URL patterns and navbar text
    - Removed redundant newline in URL patterns for seed sample update and delete.
    - Changed "Breeders" to "Entities" in the navbar dropdown.
    - Adjusted indentation for navbar brand and other elements for consistency.

--- collect/urls.py
+++ collect/urls.py
@@ -14,16 +14,8 @@ urlpatterns = [
     path("storage/<int:pk>/update", views.storage_update, name="storage_update"),
     path("storage/<int:pk>/delete", views.storage_delete, name="storage_delete"),
     path("storage/sort", views.StorageSortView.as_view(), name="storage-sort"),
-    path(
-        "<int:pk>/update",
-        views.seedsample_update,
-        name="seedsample_update",
-    ),
-    path(
-        "<int:pk>/delete",
-        views.SeedSampleDeleteView.as_view(),
-        name="seedsample_delete",
-    ),
+    path("<int:pk>/update", views.seedsample_update, name="seedsample_update"),
+    path("<int:pk>/delete", views.SeedSampleDeleteView.as_view(), name="seedsample_delete"),
     path("cart/change", views.cart_change, name="cart-change"),
     path("cart/new", views.cart_create, name="cart-create"),
     path("cart/<int:pk>/update", views.cart_update, name="cart-update"),
