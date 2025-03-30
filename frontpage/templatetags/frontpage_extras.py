fdc46fe6c4b8b05f2dbcbdab6a6976dee1283e20
Gabriele Mongiano <kofm@users.noreply.github.com>
2025-03-30 20:46:57 +0200

    Refactor description list and remove unused templates
    - Changed icons in `seedsample_table_actions.html` for better visual consistency.
    - Removed several unused templates from `frontpage` such as `_form_heading.html`, `_form_title.html`, `edit_icon.html`, and `page_title.html`.
    - Deleted unused entry in `components.yaml` related to `page_title`.
    - Edited `frontpage_extras.py` by removing the `render_edit_icon` template tag function.

--- frontpage/templatetags/frontpage_extras.py
+++ frontpage/templatetags/frontpage_extras.py
@@ -20,11 +20,6 @@ def get_proper_elided_page_range(p, number, on_each_side=3, on_ends=2):
     return paginator.get_elided_page_range(number=number, on_each_side=on_each_side, on_ends=on_ends)
 
 
-@register.inclusion_tag("frontpage/partials/edit_icon.html")
-def render_edit_icon(url):
-    return {"url": url}
-
-
 @register.filter("add_valid_class")
 @silence_without_field
 def add_valid_class(field, css_class):
