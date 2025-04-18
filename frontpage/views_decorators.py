a20a0ecc6e4eb07a4ac40124283b0aefc2e871ac
Gabriele Mongiano <kofm@users.noreply.github.com>
2025-04-18 21:05:40 +0200

    feat: add htmx block rendering decorator
    - Add `htmx_render_block_from_params` decorator to support rendering template blocks for HTMX requests
    - Implement helper functions to detect HTMX requests and extract block parameters from GET/POST
    - Add `make_get_request` utility to convert a request to GET, useful for view resets

--- frontpage/views_decorators.py
+++ frontpage/views_decorators.py
@@ -1,5 +1,76 @@
 """Project-wide decorators."""
 
+import copy
+
+from django.http.request import HttpRequest, QueryDict
+from django.http.response import HttpResponse
+from django.utils.functional import wraps
+from render_block import render_block_to_string
+
+
+def is_htmx(request: HttpRequest):
+    return request.headers.get("Hx-Request", False)
+
+
+def htmx_render_block_from_params():
+    def decorator(view):
+        @wraps(view)
+        def _view(request, *args, **kwargs):
+            resp = view(request, *args, **kwargs)
+
+            if not is_htmx(request):
+                return resp
+
+            if not hasattr(resp, "render"):
+                raise ValueError("@htmx_render_block_from_params should be used with TemplateResponse")
+
+            blocks_to_use = _get_param_from_request(request, "use_block")
+
+            if not blocks_to_use:
+                return resp
+
+            rendered_blocks = [
+                render_block_to_string(
+                    resp.template_name,
+                    b,
+                    context=resp.context_data,
+                    request=request,
+                )
+                for b in list(blocks_to_use)
+            ]
+            resp = HttpResponse(
+                content="\n".join(rendered_blocks),
+                status=resp.status_code,
+                headers=resp.headers,
+            )
+
+            return resp
+
+        return _view
+
+    return decorator
+
+
+def _get_param_from_request(request, param):
+    """
+    Checks GET then POST params for specified param
+    """
+    if param in request.GET:
+        return request.GET.getlist(param)
+    elif request.method == "POST" and param in request.POST:
+        return request.POST.getlist(param)
+    return None
+
+
+def make_get_request(request: HttpRequest) -> HttpRequest:
+    """
+    Returns a new GET request based on passed in request.
+    """
+    new_request = copy.copy(request)
+    new_request.POST = QueryDict()
+    new_request.method = "GET"
+    return new_request
+
 
 def nav_active(nav: str):
     """Add active menu item to view context.
