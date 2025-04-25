e243f7264539faedd53166d966e6d999e9fff7c9
Gabriele Mongiano <kofm@users.noreply.github.com>
2025-04-25 08:36:46 +0200

    feat: add django-browser-reload for development convenience
    - Added django-browser-reload to dev dependencies in pyproject.toml and uv.lock
    - Enabled django-browser-reload in dev.py by adding app and middleware
    - Updated persefone/urls.py to include django_browser_reload URLs under __reload__/ path
    - Fixed template usage in sample_detail.html (corrected variable in card_col_rows for storage location and weight display)

--- persefone/settings/dev.py
+++ persefone/settings/dev.py
@@ -5,11 +5,20 @@ STATIC_ROOT = "static/"
 SHELL_PLUS = "ipython"
 
 
-INSTALLED_APPS = ["debug_toolbar", "django_extensions", *INSTALLED_APPS]
+INSTALLED_APPS = [
+    "debug_toolbar",
+    "django_extensions",
+    "django_browser_reload",
+    *INSTALLED_APPS,
+]
 
 INTERNAL_IPS = ["127.0.0.1", "localhost"]
 
-MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware", *MIDDLEWARE]
+MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
+    *MIDDLEWARE,
+]
 
 DEBUG_TOOLBAR_ENABLED = True
 DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": lambda r: True}
--- persefone/urls.py
+++ persefone/urls.py
@@ -35,4 +35,5 @@ urlpatterns = [
 if settings.DEBUG and settings.DEBUG_TOOLBAR_ENABLED:
     from debug_toolbar.toolbar import debug_toolbar_urls
 
+    urlpatterns = [path("__reload__/", include("django_browser_reload.urls")), *urlpatterns]
     urlpatterns = urlpatterns + debug_toolbar_urls()
