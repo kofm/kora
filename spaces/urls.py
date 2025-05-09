4263582de34161814a9894728324b0b7f7dd9e5d
Gabriele Mongiano <kofm@users.noreply.github.com>
2025-05-09 15:22:49 +0200

    feat(spaces): refactor area duplication and reorder URL patterns
    - Refactor area duplication by merging it into area_create view, supporting duplication via query param
    - Remove unused area_duplicate view and AreaDetailView class
    - Fix area_create form initialization to handle duplication initial data correctly
    - Reorder and clean up URL patterns in spaces/urls.py for better consistency
    - Remove redundant area_sort_hx function
    - Simplify imports and correct minor code style issues
    - Update area_detail.html to link to area_create with duplication query param for duplication button

--- spaces/urls.py
+++ spaces/urls.py
@@ -6,15 +6,14 @@ app_name = "spaces"
 
 urlpatterns = [
     path("locations", views.location_list, name="location_list"),
-    path("locations/sort", views.SortLocation.as_view(), name="location_sort"),
-    path("locations/<int:location_id>/area/create", views.area_create, name="area_create"),
     path("locations/<int:pk>", views.location_detail, name="location_detail"),
+    path("locations/create", views.LocationCreateView.as_view(), name="location_create"),
     path("locations/<int:pk>/update", views.LocationUpdateView.as_view(), name="location_update"),
     path("locations/<int:pk>/delete", views.LocationDeleteView.as_view(), name="location_delete"),
-    path("locations/create", views.LocationCreateView.as_view(), name="location_create"),
-    path("areas/sort", views.AreaSort.as_view(), name="area_sort"),
+    path("locations/sort", views.SortLocation.as_view(), name="location_sort"),
     path("areas/<int:pk>", views.area_detail, name="area_detail"),
+    path("locations/<int:location_id>/area/create", views.area_create, name="area_create"),
     path("areas/<int:pk>/update", views.AreaUpdateView.as_view(), name="area_update"),
     path("areas/<int:pk>/delete", views.AreaDeleteView.as_view(), name="area_delete"),
-    path("areas/<int:pk>/duplicate", views.area_duplicate, name="area_duplicate"),
+    path("areas/sort", views.AreaSort.as_view(), name="area_sort"),
 ]
--- spaces/views.py
+++ spaces/views.py
@@ -1,17 +1,12 @@
-import json
-
-from django.db.models.base import Model
 from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
 from django.urls import reverse
 from django.urls.base import reverse_lazy
-from django.views.decorators.http import require_http_methods
-from django.views.generic import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from breadcrumbs.utils import add_parent_breadcrumbs, generate_breadcrumbs
 from django_sortable_htmx.views import SortableView
-from frontpage.utils.htmx import htmx_response_redirect, htmx_response_trigger_close_modal
+from frontpage.utils.htmx import htmx_response_trigger_close_modal
 from frontpage.views_decorators import htmx_render_blocks, nav_active
 from spaces.forms import AreaForm
 from spaces.models import Area, Location
@@ -30,26 +25,7 @@ class SortLocation(SortableView):
     model = Location
 
 
-@require_http_methods(["POST"])
-def area_sort_hx(request):
-    area_pks_ordered = request.POST.getlist("area_order")
-    location_pk = request.POST.get("location")
-    location = Location.objects.get(pk=location_pk)
-    area_list = []
-    for idx, area_pk in enumerate(area_pks_ordered, start=1):
-        area = Area.objects.get(pk=area_pk)
-        area.order = idx
-        area.location = location
-        area.save()
-        area_list.append(area)
-    return TemplateResponse(
-        request,
-        "spaces/partials/area_list.html",
-        {"area_list": area_list, "location": location},
-    )
-
-
-@htmx_render_blocks(["areas"])
+@htmx_render_blocks(("areas",))
 def location_detail(request, pk):
     location = get_object_or_404(Location, pk=pk)
 
@@ -68,15 +44,20 @@ class LocationCreateView(CreateView):
 
 def area_create(request, location_id):
     location = get_object_or_404(Location, pk=location_id)
+    duplicate = request.GET.get("duplicate", None)
+    try:
+        initial = Area.objects.values("name", "width", "length", "location_id").get(pk=duplicate) if duplicate else {}
+    except Area.DoesNotExist:
+        initial = {}
     if request.method == "POST":
-        form = AreaForm(request.POST)
+        form = AreaForm(request.POST, initial=initial)
         if form.is_valid():
             area = form.save(commit=False)
             area.location = location
             area.save()
             return htmx_response_trigger_close_modal(["areaUpdated"])
     else:
-        form = AreaForm()
+        form = AreaForm(initial=initial)
 
     context = {"form": form, "object_to_create": "Area"}
     return TemplateResponse(request, "frontpage/modal_form.html", context)
@@ -95,11 +76,6 @@ class LocationDeleteView(DeleteView):
     success_url = reverse_lazy("spaces:location_list")
 
 
-class AreaDetailView(DetailView):
-    model = Area
-    context_object_name = "area"
-
-
 @nav_active("nav_plan")
 def area_detail(request, pk):
     area = Area.objects.select_related("location").get(pk=pk)
@@ -110,40 +86,16 @@ def area_detail(request, pk):
     return TemplateResponse(request, "spaces/area_detail.html", context)
 
 
-def duplicate_object(object: Model):
-    object.pk = None
-    object._state.adding = True
-    return object
-
-
-def area_duplicate(request, pk):
-    area = Area.objects.values("width", "length", "location_id").get(pk=pk)
-    if request.method == "POST":
-        form = AreaForm(request.POST, initial=area)
-        if form.is_valid():
-            created_area = form.save(commit=False)
-            created_area.location_id = area["location_id"]
-            created_area.save()
-            response = htmx_response_redirect(reverse("spaces:area_detail", args=(created_area.pk,)))
-            response.headers["HX-Trigger"] = json.dumps({"closeModal": True})
-            return response
-    else:
-        form = AreaForm(initial=area)
-    return TemplateResponse(
-        request, "frontpage/modal_form.html", {"area": area, "object_to_create": "Area", "form": form}
-    )
-
-
 class AreaSort(SortableView):
     model = Area
 
 
 class AreaUpdateView(UpdateView):
     model = Area
-    fields = ["location", "name", "width", "length"]
+    fields = ("location", "name", "width", "length")
 
     def get_success_url(self):
-        return reverse_lazy("spaces:area_detail", args=[self.object.id])
+        return reverse_lazy("spaces:area_detail", args=(self.object.id,))
 
 
 class AreaDeleteView(DeleteView):
