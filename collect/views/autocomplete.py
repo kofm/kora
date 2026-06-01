a8ae56d308c7d72e0acb3c7c8517767fe8ee802c
Gabriele Mongiano <12545394+kofm@users.noreply.github.com>
2026-06-01 23:43:22 +0200

    fix(collect): restore position selection functionality

--- collect/views/autocomplete.py
+++ collect/views/autocomplete.py
@@ -6,7 +6,8 @@ class StoragePositionAutocompleteView(AutocompleteModelView):
     model = StoragePosition
     value_fields = ["id"]
     search_fields = ["name", "storage__name"]
+    ordering = ["storage__order", "name"]
 
     def hook_queryset(self, queryset):
-        queryset = queryset.select_related("storage")
+        queryset = queryset.select_related("storage").empty_positions_for_sample()
         return queryset
